5983b679e0f9542504418dd06e5560da44127c36
vinay0000 <vinay.rrgl@gmail.com>
2025-03-10 19:31:50 -0700

    added AbsoluteDate class

--- orbitpy/util.py
+++ orbitpy/util.py
@@ -5,3 +5,98 @@
 Collection of utility classes and functions used by the `orbitpy` package.
 Note some utility classes/functions are imported from the `instrupy` package.
 """
+
+from typing import Dict, Any
+from astropy.time import Time as Astropy_Time
+
+
+class AbsoluteDate:
+    """Handles date-time information with support to Julian and Gregorian
+    date-time formats and UT1 and UTC time scales."""
+
+    def __init__(self, astropy_time: Astropy_Time) -> None:
+        """Constructor for the AbsoluteDate class.
+
+        Args:
+            astropy_time (astropy.time.Time): Astropy Time object.
+        """
+        self.astropy_time = astropy_time
+
+    @staticmethod
+    def from_dict(dict_in: Dict[str, Any]) -> "AbsoluteDate":
+        """Construct an AbsoluteDate object from a dictionary.
+
+        Args:
+            dict_in (dict): Dictionary with the date-time information.
+                The dictionary should contain the following key-value pairs:
+                - "time_format" (str): The date-time format, either 
+                                       "GregorianDate" or "JulianDate" 
+                                       (case-insensitive).
+                - "time_scale" (str): The time scale, e.g., "utc" or "ut1"
+                                      (case-insensitive).
+
+                For "GregorianDate" format:
+                - "year" (int): The year component of the date.
+                - "month" (int): The month component of the date.
+                - "day" (int): The day component of the date.
+                - "hour" (int): The hour component of the time.
+                - "minute" (int): The minute component of the time.
+                - "second" (float): The second component of the time.
+
+                For "JulianDate" format:
+                - "jd" (float): The Julian Date.
+
+        Returns:
+            AbsoluteDate: AbsoluteDate object.
+        """
+        time_scale: str = dict_in["time_scale"].lower()
+        time_format: str = dict_in["time_format"].lower()
+        if time_format == "gregoriandate":
+            year: int = dict_in["year"]
+            month: int = dict_in["month"]
+            day: int = dict_in["day"]
+            hour: int = dict_in["hour"]
+            minute: int = dict_in["minute"]
+            second: float = dict_in["second"]
+            astropy_time = Astropy_Time(
+                f"{year}-{month:02d}-{day:02d}T{hour:02d}:{minute:02d}:{second:06.3f}",  # pylint: disable=line-too-long
+                format="isot",
+                scale=time_scale,
+            )
+        elif time_format == "juliandate":
+            jd: float = dict_in["jd"]
+            astropy_time = Astropy_Time(jd, format="jd", scale=time_scale)
+        else:
+            raise ValueError(f"Unsupported date-time format: {time_format}")
+        return AbsoluteDate(astropy_time=astropy_time)
+
+    def to_dict(self, time_format: str = "GregorianDate") -> Dict[str, Any]:
+        """Convert the AbsoluteDate object to a dictionary.
+
+        Args:
+            time_format (str): The type of date-time format to use
+                                ("GregorianDate" or "JulianDate").
+
+        Returns:
+            dict: Dictionary with the date-time information.
+        """
+        time_format = time_format.lower()
+        if time_format == "gregoriandate":
+            return {
+                "time_format": "GregorianDate",
+                "year": self.astropy_time.datetime.year,
+                "month": self.astropy_time.datetime.month,
+                "day": self.astropy_time.datetime.day,
+                "hour": self.astropy_time.datetime.hour,
+                "minute": self.astropy_time.datetime.minute,
+                "second": self.astropy_time.datetime.second,
+                "time_scale": self.astropy_time.scale,
+            }
+        elif time_format == "juliandate":
+            return {
+                "time_format": "JulianDate",
+                "jd": self.astropy_time.jd,
+                "time_scale": self.astropy_time.scale,
+            }
+        else:
+            raise ValueError(f"Unsupported date-time format: {time_format}")
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,8 +1,99 @@
 """Unit tests for orbitpy.util module."""
 
 import unittest
+from orbitpy.util import AbsoluteDate
+from astropy.time import Time as Astropy_Time
 
 
-class TestUtil(unittest.TestCase):
-    def test_sample(self):
-        self.assertEqual(1, 1)
+class TestAbsoluteDate(unittest.TestCase):
+    """ Test the AbsoluteDate class. """
+    def test_from_dict_gregorian(self):
+        dict_in = {
+            "time_format": "GregorianDate",
+            "year": 2025,
+            "month": 3,
+            "day": 10,
+            "hour": 14,
+            "minute": 30,
+            "second": 0.0,
+            "time_scale": "utc",
+        }
+        absolute_date = AbsoluteDate.from_dict(dict_in)
+        self.assertEqual(
+            absolute_date.astropy_time.iso, "2025-03-10 14:30:00.000"
+        )
+
+    def test_from_dict_julian(self):
+        dict_in = {
+            "time_format": "JulianDate",
+            "jd": 2457081.10417,
+            "time_scale": "utc",
+        }
+        absolute_date = AbsoluteDate.from_dict(dict_in)
+        self.assertAlmostEqual(
+            absolute_date.astropy_time.jd, 2457081.10417, places=5
+        )
+
+    def test_to_dict_gregorian(self):
+        astropy_time = Astropy_Time(
+            "2025-03-10T14:30:00", format="isot", scale="utc"
+        )
+        absolute_date = AbsoluteDate(astropy_time)
+        dict_out = absolute_date.to_dict("GregorianDate")
+        expected_dict = {
+            "time_format": "GregorianDate",
+            "year": 2025,
+            "month": 3,
+            "day": 10,
+            "hour": 14,
+            "minute": 30,
+            "second": 0,
+            "time_scale": "utc",
+        }
+        self.assertEqual(dict_out, expected_dict)
+
+    def test_to_dict_julian(self):
+        astropy_time = Astropy_Time(2457081.10417, format="jd", scale="utc")
+        absolute_date = AbsoluteDate(astropy_time)
+        dict_out = absolute_date.to_dict("JulianDate")
+        expected_dict = {
+            "time_format": "JulianDate",
+            "jd": 2457081.10417,
+            "time_scale": "utc",
+        }
+        self.assertEqual(dict_out, expected_dict)
+
+    def test_gregorian_to_julian(self):
+        # Initialize with Gregorian date
+        dict_in = {
+            "time_format": "GregorianDate",
+            "year": 2025,
+            "month": 3,
+            "day": 11,
+            "hour": 1,
+            "minute": 23,
+            "second": 37.0,
+            "time_scale": "ut1",
+        }
+        absolute_date = AbsoluteDate.from_dict(dict_in)
+        # Validation data from: https://aa.usno.navy.mil/data/JulianDate
+        self.assertAlmostEqual(
+            absolute_date.astropy_time.jd, 2460745.558067, places=5
+        )
+
+    def test_julian_to_gregorian(self):
+        # Initialize with Julian Date
+        dict_in = {
+            "time_format": "JulianDate",
+            "jd": 2460325.145250,
+            "time_scale": "ut1",
+        }
+        absolute_date = AbsoluteDate.from_dict(dict_in)
+        # Validation data from: https://aa.usno.navy.mil/data/JulianDate
+        self.assertEqual(
+            absolute_date.astropy_time.iso, "2024-01-15 15:29:09.600"
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
