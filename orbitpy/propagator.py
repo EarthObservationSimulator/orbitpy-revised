4e258e402ff5f6b95fcb1785fdeea9797c0c1f94
vinay0000 <vinay.rrgl@gmail.com>
2025-04-16 16:14:31 -0700

    Refactor PropagatorFactory to use PropagatorType enum and update unit tests

--- orbitpy/propagator.py
+++ orbitpy/propagator.py
@@ -5,10 +5,14 @@
 
 from typing import Type, Dict, Any, Union
 
+from eosimutils.base import EnumBase
 from eosimutils.time import AbsoluteDate
 
 from .orbits import TwoLineElementSet, OrbitalMeanElementsMessage
 
+class PropagatorType(EnumBase):
+    """Enumeration of supported propagator types."""
+    SGP4_PROPAGATOR = "SGP4_PROPAGATOR"
 
 class PropagatorFactory:
     """Factory class to register and invoke the appropriate propagator class.
@@ -17,20 +21,19 @@ class PropagatorFactory:
     of the appropriate propagator based on specifications.
 
     Attributes:
-        _creators (Dict[str, Type]): A dictionary mapping propagator type labels 
-                                     to their respective classes.
+        _creators (Dict[str, Type]): A dictionary mapping propagator type labels to their respective classes.
     """
 
     def __init__(self):
         """Initializes the PropagatorFactory and registers default propagators."""
         self._creators: Dict[str, Type] = {}
-        self.register_propagator("SGP4 PROPAGATOR", SGP4Propagator)
+        self.register_propagator(PropagatorType.SGP4_PROPAGATOR.value, SGP4Propagator)
 
     def register_propagator(self, propagator_type: str, creator: Type) -> None:
         """Registers a propagator class with a specific type label.
 
         Args:
-            _type (str): The label for the propagator type.
+            propagator_type (str): The label for the propagator type.
             creator (Type): The propagator class to register.
         """
         self._creators[propagator_type] = creator
@@ -41,24 +44,58 @@ class PropagatorFactory:
         Args:
             specs (Dict[str, Any]): A dictionary containing propagator specifications.
                 Must include a valid propagator type in the "propagator_type" key.
-                The other keys depend on the specific propagator class being requested.
-                For example, for SGP4 PROPAGATOR, the "step_size" key is expected.
 
         Returns:
-            Any: An instance of the appropriate propagator class 
-                 initialized with the given specifications.
+            Any: An instance of the appropriate propagator class initialized with the given specifications.
 
         Raises:
             KeyError: If the "propagator_type" key is missing in the specifications dictionary.
             ValueError: If the specified propagator type is not registered.
         """
-        propagator_type = specs.get("propagator_type")
-        if propagator_type is None:
+        propagator_type_str = specs.get("propagator_type")
+        if propagator_type_str is None:
             raise KeyError(
                 'Propagator type key "propagator_type" not found in specifications dictionary.'
             )
 
-        creator = self._creators.get(propagator_type)
-        if not creator:
-            raise ValueError(f'Propagator type "{propagator_type}" is not registered.')
+        if propagator_type_str not in self._creators:
+            raise ValueError(f'Propagator type "{propagator_type_str}" is not registered.')
+
+        creator = self._creators[propagator_type_str]
         return creator.from_dict(specs)
+
+class SGP4Propagator():
+   """A Simplified General Perturbations 4 (SGP4) orbit propagator class.
+    THe implementation of this class is a wrapper around the Skyfield SGP4 orbit propagator.
+
+    The instance variable(s) correspond to the propagator setting(s). 
+
+    :ivar stepSize: Orbit propagation time-step in seconds.
+    :vartype stepSize: float or None
+
+    :ivar propagator_type: Type of propagator. Value is always 'SGP4 PROPAGATOR'.
+    :vartype propagator_type: str
+
+    """
+   def __init__(self, stepSize=None):
+      self.stepSize = float(stepSize) if stepSize is not None else None
+      self.propagator_type = "SGP4_PROPAGATOR"
+   
+   @classmethod
+   def from_dict(cls, specs):
+      """ Parses an SGP4Propagator object from a dictionary.
+        
+        :param d: Dictionary with the SGP4 PROPAGATOR specifications.
+
+                Following keys are to be specified.
+                
+                * "stepSize": (float) Step size in seconds. Default value is 60s.
+
+        :paramtype d: dict
+
+        :return: ``SGP4Propagator`` object.
+        :rtype: :class:`orbitpy.propagator.SGP4Propagator`
+
+        """ 
+      step_size = specs.get("step_size", 60.0)
+      return cls(stepSize=step_size)
--- tests/test_propagator.py
+++ tests/test_propagator.py
@@ -21,26 +21,26 @@ class TestPropagatorFactory(unittest.TestCase):
         factory = PropagatorFactory()
 
         # Test the built-in propagators are registered
-        self.assertIn("SGP4 PROPAGATOR", factory._creators) # pylint: disable=protected-access
-        self.assertEqual(factory._creators["SGP4 PROPAGATOR"], SGP4Propagator) # pylint: disable=protected-access
+        self.assertIn("SGP4_PROPAGATOR", factory._creators) # pylint: disable=protected-access
+        self.assertEqual(factory._creators["SGP4_PROPAGATOR"], SGP4Propagator) # pylint: disable=protected-access
 
     def test_register_propagator(self):
         """Test registering a new propagator in the factory."""
         factory = PropagatorFactory()
         factory.register_propagator(
-            "New Propagator", TestPropagatorFactory.DummyNewPropagator
+            "New_Propagator", TestPropagatorFactory.DummyNewPropagator
         )
 
         # Verify the new propagator is registered
-        self.assertIn("New Propagator", factory._creators) # pylint: disable=protected-access
+        self.assertIn("New_Propagator", factory._creators) # pylint: disable=protected-access
         self.assertEqual(
-            factory._creators["New Propagator"], # pylint: disable=protected-access
+            factory._creators["New_Propagator"], # pylint: disable=protected-access
             TestPropagatorFactory.DummyNewPropagator,
         )
 
         # Verify the built-in propagators remain registered
-        self.assertIn("SGP4 PROPAGATOR", factory._creators) # pylint: disable=protected-access
-        self.assertEqual(factory._creators["SGP4 PROPAGATOR"], SGP4Propagator) # pylint: disable=protected-access
+        self.assertIn("SGP4_PROPAGATOR", factory._creators) # pylint: disable=protected-access
+        self.assertEqual(factory._creators["SGP4_PROPAGATOR"], SGP4Propagator) # pylint: disable=protected-access
 
     def test_get_propagator(self):
         """Test retrieving propagators based on specifications."""
@@ -48,17 +48,17 @@ class TestPropagatorFactory(unittest.TestCase):
 
         # Register dummy propagator
         factory.register_propagator(
-            "New Propagator", TestPropagatorFactory.DummyNewPropagator
+            "New_Propagator", TestPropagatorFactory.DummyNewPropagator
         )
 
         # Test SGP4 PROPAGATOR retrieval
-        specs = {"propagator_type": "SGP4 PROPAGATOR", "step_size": 60}
+        specs = {"propagator_type": "SGP4_PROPAGATOR", "step_size": 60}
         sgp4_prop = factory.get_propagator(specs)
         self.assertIsInstance(sgp4_prop, SGP4Propagator)
 
         # Test DummyNewPropagator retrieval
         specs = {
-            "propagator_type": "New Propagator"
+            "propagator_type": "New_Propagator"
         }  # in practice additional propagator specs shall be present in the dictionary
         new_prop = factory.get_propagator(specs)
         self.assertIsInstance(
@@ -75,4 +75,4 @@ class TestPropagatorFactory(unittest.TestCase):
 
         # Test unregistered propagator type
         with self.assertRaises(ValueError):
-            factory.get_propagator({"propagator_type": "Invalid Propagator"})
+            factory.get_propagator({"propagator_type": "Invalid_Propagator"})
