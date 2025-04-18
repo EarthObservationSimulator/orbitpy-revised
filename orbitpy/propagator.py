301540b59547ade79e39195bcbe86177177d2d61
vinay0000 <vinay.rrgl@gmail.com>
2025-04-17 17:46:45 -0700

    wip: SGP4 propagator

--- orbitpy/propagator.py
+++ orbitpy/propagator.py
@@ -4,15 +4,27 @@
 """
 
 from typing import Type, Dict, Any, Union
+import math
 
-from eosimutils.base import EnumBase
-from eosimutils.time import AbsoluteDate
+from skyfield.api import (
+    EarthSatellite as Skyfield_EarthSatellite,
+    load as Skyfield_load,
+)
+
+from eosimutils.base import EnumBase, ReferenceFrame
+from eosimutils.time import AbsoluteDate, AbsoluteDateArray
+from eosimutils.trajectory import StateSeries
 
 from .orbits import TwoLineElementSet, OrbitalMeanElementsMessage
 
+NUMBER_OF_SECONDS_IN_A_DAY = 86400.0
+
+
 class PropagatorType(EnumBase):
     """Enumeration of supported propagator types."""
+
     SGP4_PROPAGATOR = "SGP4_PROPAGATOR"
+
 
 class PropagatorFactory:
     """Factory class to register and invoke the appropriate propagator class.
@@ -27,13 +39,16 @@ class PropagatorFactory:
         propagator = factory.get_propagator(specs)
 
     Attributes:
-        _creators (Dict[str, Type]): A dictionary mapping propagator type labels to their respective classes.
+        _creators (Dict[str, Type]): A dictionary mapping propagator type 
+                                     labels to their respective classes.
     """
 
     def __init__(self):
         """Initializes the PropagatorFactory and registers default propagators."""
         self._creators: Dict[str, Type] = {}
-        self.register_propagator(PropagatorType.SGP4_PROPAGATOR.value, SGP4Propagator)
+        self.register_propagator(
+            PropagatorType.SGP4_PROPAGATOR.value, SGP4Propagator
+        )
 
     def register_propagator(self, propagator_type: str, creator: Type) -> None:
         """Registers a propagator class with a specific type label.
@@ -52,7 +67,8 @@ class PropagatorFactory:
                 Must include a valid propagator type in the "propagator_type" key.
 
         Returns:
-            Any: An instance of the appropriate propagator class initialized with the given specifications.
+            Any: An instance of the appropriate propagator class initialized 
+                 with the given specifications.
 
         Raises:
             KeyError: If the "propagator_type" key is missing in the specifications dictionary.
@@ -65,43 +81,127 @@ class PropagatorFactory:
             )
 
         if propagator_type_str not in self._creators:
-            raise ValueError(f'Propagator type "{propagator_type_str}" is not registered.')
+            raise ValueError(
+                f'Propagator type "{propagator_type_str}" is not registered.'
+            )
 
         creator = self._creators[propagator_type_str]
         return creator.from_dict(specs)
 
-class SGP4Propagator():
-   """A Simplified General Perturbations 4 (SGP4) orbit propagator class.
-    THe implementation of this class is a wrapper around the Skyfield SGP4 orbit propagator.
 
-    The instance variable(s) correspond to the propagator setting(s). 
+class SGP4Propagator:
+    """A Simplified General Perturbations 4 (SGP4) orbit propagator class.
 
-    :ivar stepSize: Orbit propagation time-step in seconds.
-    :vartype stepSize: float or None
+    This class is a wrapper around the Skyfield SGP4 orbit propagator.
 
-    :ivar propagator_type: Type of propagator. Value is always 'SGP4 PROPAGATOR'.
-    :vartype propagator_type: str
-
+    Attributes:
+        step_size (float): Orbit propagation time-step in seconds. Default is 60 seconds.
+        propagator_type (str): Type of propagator. Value is always 'SGP4_PROPAGATOR'.
     """
-   def __init__(self, stepSize=None):
-      self.stepSize = float(stepSize) if stepSize is not None else None
-      self.propagator_type = "SGP4_PROPAGATOR"
-   
-   @classmethod
-   def from_dict(cls, specs):
-      """ Parses an SGP4Propagator object from a dictionary.
-        
-        :param d: Dictionary with the SGP4 PROPAGATOR specifications.
 
-                Following keys are to be specified.
-                
-                * "stepSize": (float) Step size in seconds. Default value is 60s.
+    def __init__(self, step_size=None):
+        """Initializes the SGP4Propagator.
 
-        :paramtype d: dict
+        Args:
+            step_size (float, optional): Orbit propagation time-step in seconds. 
+                                         Default is 60 seconds.
+        """
+        self.step_size = float(step_size) if step_size is not None else 60.0
+        self.propagator_type = "SGP4_PROPAGATOR"
 
-        :return: ``SGP4Propagator`` object.
-        :rtype: :class:`orbitpy.propagator.SGP4Propagator`
+    @classmethod
+    def from_dict(cls, specs):
+        """Parses an SGP4Propagator object from a dictionary.
 
-        """ 
-      step_size = specs.get("step_size", 60.0)
-      return cls(stepSize=step_size)
+        Args:
+            specs (dict): Dictionary with the SGP4 propagator specifications.
+                The following keys are expected:
+                - "step_size" (float): Step size in seconds. Default value is 60 seconds.
+
+        Returns:
+            SGP4Propagator: An instance of the SGP4Propagator class.
+        """
+        step_size = specs.get("step_size", 60.0)
+        return cls(step_size=step_size)
+
+    def execute(
+        self,
+        t0: AbsoluteDate,
+        duration_days: float,
+        orbit: Union[TwoLineElementSet, OrbitalMeanElementsMessage],
+    ) -> StateSeries:
+        """Propagates the orbit from t0 to t1 using the SGP4 propagator.
+
+        Args:
+            t0 (AbsoluteDate): Start time for propagation.
+            duration_days (float): Duration of propagation in days.
+            orbit (Union[TwoLineElementSet, OrbitalMeanElementsMessage]): 
+                    Orbital specifications to be propagated.
+                    This can be either a TLE set or an Orbital Mean Elements Message.
+
+        Returns:
+            StateSeries: A StateSeries object containing the propagated trajectory.
+
+        Raises:
+            ValueError: If the orbit type is invalid for SGP4 propagation.
+        """
+        if isinstance(orbit, TwoLineElementSet) or isinstance(
+            orbit, OrbitalMeanElementsMessage
+        ):
+            skyfield_ts = Skyfield_load.timescale()
+
+            skyfield_t0 = t0.to_skyfield_time()
+            skyfield_t1 = (
+                t0 + duration_days * NUMBER_OF_SECONDS_IN_A_DAY
+            ).to_skyfield_time()
+            propagate_time = skyfield_ts.linspace(
+                skyfield_t0,
+                skyfield_t1,
+                num=math.floor(
+                    duration_days * NUMBER_OF_SECONDS_IN_A_DAY / self.step_size
+                )
+                + 1,
+            )
+
+            tle = orbit.get_tle_as_tuple()
+            skyfield_sat = Skyfield_EarthSatellite(
+                tle[0], tle[1], None, skyfield_ts
+            )
+
+            geocentric = skyfield_sat.at(
+                propagate_time
+            )  # in GCRS (ECI) coordinates
+            pos = geocentric.position.km
+            vel = geocentric.velocity.km_per_s
+
+            # Convert Skyfield Time object to AbsoluteDateArray
+            absolute_date_array = AbsoluteDateArray.from_dict(
+                {
+                    "time_format": "Gregorian_Date",
+                    "times": propagate_time.utc_strftime(
+                        format="%Y-%m-%dT%H:%M:%S"
+                    ),
+                    "time_scale": "utc",
+                }
+            )
+
+            # Define the reference frame. GCRS of Skyfield is considered
+            # equivalent to ReferenceFrame.ICRF_EC
+            # See: https://rhodesmill.org/skyfield/earth-satellites.html#generating-a-satellite-position # pylint: disable=line-too-long
+            reference_frame = ReferenceFrame.ICRF_EC
+
+            # Instantiate the StateSeries object
+            state_series = StateSeries(
+                time=absolute_date_array,
+                data=[
+                    pos.T,
+                    vel.T,
+                ],  # Transpose pos and vel to match StateSeries format
+                frame=reference_frame,
+            )
+            return state_series
+
+        else:
+            raise ValueError(
+                "Invalid orbit type for SGP4 propagation. Must be TLE or mean elements message."
+            )
--- tests/test_propagator.py
+++ tests/test_propagator.py
@@ -1,11 +1,18 @@
 """Unit tests for orbitpy.propagator module."""
 
 import unittest
+import math
+
+from eosimutils.time import AbsoluteDate
+from eosimutils.trajectory import StateSeries
+
 from orbitpy.propagator import PropagatorFactory, SGP4Propagator
+from orbitpy.orbits import TwoLineElementSet
 
 
 class TestPropagatorFactory(unittest.TestCase):
     """Tests for the PropagatorFactory class."""
+
     class DummyNewPropagator:
         """A dummy propagator class for testing purposes."""
 
@@ -21,8 +28,12 @@ class TestPropagatorFactory(unittest.TestCase):
         factory = PropagatorFactory()
 
         # Test the built-in propagators are registered
-        self.assertIn("SGP4_PROPAGATOR", factory._creators) # pylint: disable=protected-access
-        self.assertEqual(factory._creators["SGP4_PROPAGATOR"], SGP4Propagator) # pylint: disable=protected-access
+        self.assertIn(
+            "SGP4_PROPAGATOR", factory._creators  # pylint: disable=protected-access
+        )
+        self.assertEqual(
+            factory._creators["SGP4_PROPAGATOR"], SGP4Propagator # pylint: disable=protected-access
+        )
 
     def test_register_propagator(self):
         """Test registering a new propagator in the factory."""
@@ -32,15 +43,23 @@ class TestPropagatorFactory(unittest.TestCase):
         )
 
         # Verify the new propagator is registered
-        self.assertIn("New_Propagator", factory._creators) # pylint: disable=protected-access
+        self.assertIn(
+            "New_Propagator", factory._creators # pylint: disable=protected-access
+        )
         self.assertEqual(
-            factory._creators["New_Propagator"], # pylint: disable=protected-access
+            factory._creators[ # pylint: disable=protected-access
+                "New_Propagator"
+            ],
             TestPropagatorFactory.DummyNewPropagator,
         )
 
         # Verify the built-in propagators remain registered
-        self.assertIn("SGP4_PROPAGATOR", factory._creators) # pylint: disable=protected-access
-        self.assertEqual(factory._creators["SGP4_PROPAGATOR"], SGP4Propagator) # pylint: disable=protected-access
+        self.assertIn(
+            "SGP4_PROPAGATOR", factory._creators # pylint: disable=protected-access
+        )
+        self.assertEqual(
+            factory._creators["SGP4_PROPAGATOR"], SGP4Propagator # pylint: disable=protected-access
+        )
 
     def test_get_propagator(self):
         """Test retrieving propagators based on specifications."""
@@ -76,3 +95,74 @@ class TestPropagatorFactory(unittest.TestCase):
         # Test unregistered propagator type
         with self.assertRaises(ValueError):
             factory.get_propagator({"propagator_type": "Invalid_Propagator"})
+
+
+class TestSGP4Propagator(unittest.TestCase):
+    """Unit tests for the SGP4Propagator class."""
+
+    def setUp(self):
+        """Set up test data for SGP4Propagator."""
+        self.step_size = 60  # seconds
+        self.propagator = SGP4Propagator(step_size=self.step_size)
+        self.tle = TwoLineElementSet(
+            line0="0 TEST SATELLITE",
+            line1="1 25544U 98067A   21275.48835648  .00001264  00000-0  29621-4 0  9993",
+            line2="2 25544  51.6453  21.5554 0002287  45.1234 315.6789 15.48912345678901",
+        )
+        self.start_time = AbsoluteDate.from_dict(
+            {
+                "time_format": "Gregorian_Date",
+                "calendar_date": "2025-03-17T12:00:00",
+                "time_scale": "utc",
+            }
+        )
+        self.duration_days = 1  # Propagate for 1 days (24 hours)
+
+    def test_execute_with_tle(self):
+        """Test the execute method with a TwoLineElementSet orbit."""
+        result = self.propagator.execute(
+            t0=self.start_time, duration_days=self.duration_days, orbit=self.tle
+        )
+
+        # Verify the result is a StateSeries object
+        self.assertIsInstance(result, StateSeries)
+
+        # Verify the time array in the StateSeries
+        self.assertEqual(
+            len(result.time),
+            math.floor(self.duration_days * 86400.0 / self.step_size) + 1,
+        )
+        self.assertEqual(result.time[0], self.start_time)
+
+        # below test has almost equal and not "equal" because of the way the
+        # time steps over the specified duration are calculated
+        self.assertAlmostEqual(
+            result.time[-1].to_spice_ephemeris_time(),
+            (
+                self.start_time + (self.duration_days * 86400.0)
+            ).to_spice_ephemeris_time(),
+            places=3,
+        )
+
+        # Verify the position and velocity arrays
+        self.assertEqual(
+            result.data[0].shape, (len(result.time), 3)
+        )  # Position array
+        self.assertEqual(
+            result.data[1].shape, (len(result.time), 3)
+        )  # Velocity array
+
+        # Verify that the reference frame is set to ICRF_EC
+        self.assertEqual(result.frame, "ICRF_EC")
+
+    def test_execute_invalid_orbit(self):
+        """Test the execute method with an invalid orbit type."""
+        with self.assertRaises(ValueError) as context:
+            self.propagator.execute(
+                t0=self.start_time,
+                duration_days=self.duration_days,
+                orbit="InvalidOrbit",
+            )
+        self.assertIn(
+            "Invalid orbit type for SGP4 propagation", str(context.exception)
+        )
