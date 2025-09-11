4c6444ad8363d641a6c8016141c92a924a3fc43c
vinay0000 <vinay.rrgl@gmail.com>
2025-09-11 11:19:50 -0700

    Abstract out a helper function to get entity positions as numpy arrays and refactor LineOfSightContactFinder to use it

--- orbitpy/contactfinder.py
+++ orbitpy/contactfinder.py
@@ -171,6 +171,168 @@ class ContactFinderFactory:
             )
         return contact_finder_class.from_dict(specs)
 
+def get_entities_position_as_numpy(
+        frame_graph: FrameGraph,
+        entity1_state: Union[
+            StateSeries,
+            PositionSeries,
+            CartesianState,
+            GeographicPosition,
+            Cartesian3DPosition,
+        ],
+        entity2_state: Union[
+            StateSeries,
+            PositionSeries,
+            CartesianState,
+            GeographicPosition,
+            Cartesian3DPosition,
+        ]) -> tuple[bool, np.ndarray, bool, np.ndarray]:
+    """Helper function to extract position as numpy arrays for entities.
+    The positions are returned in a common reference frame.
+
+    Presently only supports the scenarios when:
+        - Both entities are fixed, at the same reference frame.
+        - At least one of the entities is fixed (in a reference frame) while
+            the other entity is in motion.
+
+    Args:
+        frame_graph (FrameGraph): The frame graph containing transformations between
+                                        reference frames.
+        entity1_state (Union[StateSeries, PositionSeries, CartesianState,
+                                GeographicPosition, Cartesian3DPosition]): State or position
+                                                                        of the first entity.
+        entity2_state (Union[StateSeries, PositionSeries, CartesianState,
+                                GeographicPosition, Cartesian3DPosition]): State or position
+                                                                        of the second entity.
+
+    Returns:
+        tuple: A tuple containing:
+            - bool: True if the first entity is fixed, False otherwise.
+            - np.ndarray: Numpy array of position(s) for the first entity. 
+                        Multiple positions are returned as a numpy array of shape (N, 3).
+            - bool: True if the second entity is fixed, False otherwise.
+            - np.ndarray: Numpy array of position(s) for the second entity. 
+                        Multiple positions are returned as a numpy array of shape (N, 3).
+
+    Raises:
+        NotImplementedError: If a particular scenario is not supported 
+                        (e.g., fixed entities in different reference frames).
+    """
+    entity1_fixed_flag = False
+    entity2_fixed_flag = False
+
+    # Convert all the entities to Cartesian3DPosition (fixed) or PositionSeries (moving)
+    if isinstance(
+        entity1_state,
+        (GeographicPosition, Cartesian3DPosition, CartesianState),
+    ):
+        entity1_fixed_flag = True
+        entity1_state = eosimutils.utils.convert_object(
+            source_obj=entity1_state, target_type=Cartesian3DPosition
+        )
+    elif isinstance(entity1_state, (StateSeries, PositionSeries)):
+        entity1_fixed_flag = False
+        entity1_state = eosimutils.utils.convert_object(
+            source_obj=entity1_state, target_type=PositionSeries
+        )
+
+    if isinstance(
+        entity2_state,
+        (GeographicPosition, Cartesian3DPosition, CartesianState),
+    ):
+        entity2_fixed_flag = True
+        entity2_state = eosimutils.utils.convert_object(
+            source_obj=entity2_state, target_type=Cartesian3DPosition
+        )
+    elif isinstance(entity2_state, (StateSeries, PositionSeries)):
+        entity2_fixed_flag = False
+        entity2_state = eosimutils.utils.convert_object(
+            source_obj=entity2_state, target_type=PositionSeries
+        )
+
+    #### Handle the scenario when both the entities are fixed. ####
+    if entity1_fixed_flag and entity2_fixed_flag:
+        if entity1_state.frame != entity2_state.frame:
+            raise NotImplementedError(
+                "Both the fixed entities must be fixed in the same reference frame."
+            )
+        else:
+            entity1_position_np = entity1_state.to_numpy()
+            entity2_position_np = entity2_state.to_numpy()
+            return (
+                entity1_fixed_flag,
+                entity1_position_np,
+                entity2_fixed_flag,
+                entity2_position_np,
+        )
+
+    #### Handle the scenario when both the entities are moving. ####
+    if not (entity1_fixed_flag or entity2_fixed_flag):
+        raise NotImplementedError(
+            "At least one of the entities must be of type GeographicPosition"
+            " or Cartesian3DPosition, i.e. it has to be fixed."
+        )
+
+    #### Handle the scenario when one of the entities is moving (logical XOR operation). ####
+    if (entity1_fixed_flag and not entity2_fixed_flag) or (not entity1_fixed_flag and entity2_fixed_flag):
+        # The fixed entity is Cartesian3DPosition type and the moving entity is PositionSeries type.
+        if entity1_fixed_flag is False:
+            moving_entity_position_series = entity1_state
+            fixed_entity_cartesian_3d_position = entity2_state
+        else:
+            moving_entity_position_series = entity2_state
+            fixed_entity_cartesian_3d_position = entity1_state
+
+        # Helper function to transform position vectors to a target frame
+        def transform_to_target_frame(
+            frame_graph, from_frame, to_frame, input_pos_vector, times
+        ):
+            """Transform the input position vector to the target frame."""
+            # TODO: Should the position transform be also applied? I.e. should
+            # the relative position difference in the center of the from and to
+            # frames be considered?
+            rot_array, _ = frame_graph.get_orientation_transform(
+                from_frame, to_frame, times
+            )
+            return rot_array.apply(input_pos_vector)
+
+        if (
+            fixed_entity_cartesian_3d_position.frame
+            != moving_entity_position_series.frame
+        ):
+            # Transform the moving entity's state to the fixed entity's reference frame.
+            # The reason the moving entity is transformed is because the time information
+            # is readily available with the moving entity's position series object.
+            moving_entity_position_np = transform_to_target_frame(
+                frame_graph,
+                moving_entity_position_series.frame,
+                fixed_entity_cartesian_3d_position.frame,
+                moving_entity_position_series.position.to_numpy(),
+                moving_entity_position_series.time,
+            )
+        else:
+            moving_entity_position_np = (
+                moving_entity_position_series.position.to_numpy()
+            )
+
+        fixed_entity_position_np = fixed_entity_cartesian_3d_position.to_numpy()
+
+        if entity1_fixed_flag is True:
+            return (
+                True,
+                fixed_entity_position_np,
+                False,
+                moving_entity_position_np,
+            )
+        else:
+            return (
+                False,
+                moving_entity_position_np,
+                True,
+                fixed_entity_position_np,
+            )
+
+    
 
 @ContactFinderFactory.register_type(
     ContactFinderType.LOS_CONTACT_FINDER.to_string()
@@ -228,113 +390,50 @@ class LineOfSightContactFinder:
             ContactIntervals: An object containing time-intervals of contact opportunities.
         """
 
-        # Convert all the entities to Cartesian3DPosition (fixed) or PositionSeries (moving)
-        entity1_fixed = False
-        entity2_fixed = False
-
-        if isinstance(
-            entity1_state,
-            (GeographicPosition, Cartesian3DPosition, CartesianState),
-        ):
-            entity1_fixed = True
-            entity1_state = eosimutils.utils.convert_object(
-                source_obj=entity1_state, target_type=Cartesian3DPosition
-            )
-        elif isinstance(entity1_state, (StateSeries, PositionSeries)):
-            entity1_fixed = False
-            entity1_state = eosimutils.utils.convert_object(
-                source_obj=entity1_state, target_type=PositionSeries
-            )
-
-        if isinstance(
-            entity2_state,
-            (GeographicPosition, Cartesian3DPosition, CartesianState),
-        ):
-            entity2_fixed = True
-            entity2_state = eosimutils.utils.convert_object(
-                source_obj=entity2_state, target_type=Cartesian3DPosition
-            )
-        elif isinstance(entity2_state, (StateSeries, PositionSeries)):
-            entity2_fixed = False
-            entity2_state = eosimutils.utils.convert_object(
-                source_obj=entity2_state, target_type=PositionSeries
-            )
-
-        if not (entity1_fixed or entity2_fixed):
-            raise NotImplementedError(
-                "At least one of the entities must be of type GeographicPosition"
-                " or Cartesian3DPosition, i.e. it has to be fixed."
-            )
+        entity1_fixed_flag, entity1_state_np, entity2_fixed_flag, entity2_state_np = get_entities_position_as_numpy(
+            frame_graph, entity1_state, entity2_state
+        )
 
         #### Handle the scenario when both the entities are fixed. ####
-        if entity1_fixed and entity2_fixed:
-            if entity1_state.frame != entity2_state.frame:
-                raise NotImplementedError(
-                    "Both the fixed entities must be fixed in the same reference frame."
-                )
-            # Both entities are fixed in the same reference frame, so we can
-            # proceed with the calculations.
+        if entity1_fixed_flag and entity2_fixed_flag:
             los = utils.check_line_of_sight(
-                entity1_state.to_numpy(),
-                entity2_state.to_numpy(),
+                entity1_state_np,
+                entity2_state_np,
                 WGS84_EARTH_POLAR_RADIUS,
             )
             return ContactInfo(None, np.array([los], dtype=bool))
+    
 
-        #### Handle the scenario when one of the entities is moving. ####
-        # The fixed entity is Cartesian3DPosition type and the moving entity is PositionSeries type.
-        if entity1_fixed is False:
-            moving_entity_position_series = entity1_state
-            fixed_entity_cartesian_3d_position = entity2_state
-        else:
-            moving_entity_position_series = entity2_state
-            fixed_entity_cartesian_3d_position = entity1_state
-
-        # Helper function to transform position vectors to a target frame
-        def transform_to_target_frame(
-            frame_graph, from_frame, to_frame, input_pos_vector, times
-        ):
-            """Transform the input position vector to the target frame."""
-            # TODO: Should the position transform be also applied? I.e. should
-            # the relative position difference in the center of the from and to
-            # frames be considered?
-            rot_array, _ = frame_graph.get_orientation_transform(
-                from_frame, to_frame, times
-            )
-            return rot_array.apply(input_pos_vector)
-
-        if (
-            fixed_entity_cartesian_3d_position.frame
-            != moving_entity_position_series.frame
-        ):
-            # Transform the moving entity's state to the fixed entity's reference frame.
-            # The reason the moving entity is transformed is because the time information
-            # is readily available with the moving entity's position series object.
-            moving_entity_position_np = transform_to_target_frame(
-                frame_graph,
-                moving_entity_position_series.frame,
-                fixed_entity_cartesian_3d_position.frame,
-                moving_entity_position_series.position.to_numpy(),
-                moving_entity_position_series.time,
-            )
-        else:
-            moving_entity_position_np = (
-                moving_entity_position_series.position.to_numpy()
+        #### Handle the scenario when both the entities are moving. ####
+        if not (entity1_fixed_flag or entity2_fixed_flag):
+            raise NotImplementedError(
+                "The scenario where both the observer and target are moving is not supported."
             )
 
-        # Check for line-of-sight contact opportunities
-        los = []
-        fixed_entity_position_np = fixed_entity_cartesian_3d_position.to_numpy()
-        for _, cart_position_np in enumerate(moving_entity_position_np):
-            los.append(
-                utils.check_line_of_sight(
-                    fixed_entity_position_np,
-                    cart_position_np,
-                    WGS84_EARTH_POLAR_RADIUS,
+        #### Handle the scenario when one of the entities is moving (logical XOR operation). ####
+        if (entity1_fixed_flag and not entity2_fixed_flag) or (not entity1_fixed_flag and entity2_fixed_flag):
+            if entity1_fixed_flag is False:
+                moving_entity_position_series = entity1_state
+                moving_entity_position_np = entity1_state_np
+                fixed_entity_position_np = entity2_state_np
+            else:
+                moving_entity_position_series = entity2_state
+                moving_entity_position_np = entity2_state_np
+                fixed_entity_position_np = entity1_state_np
+
+            # Check for line-of-sight contact opportunities
+            los = []
+            
+            for _, cart_position_np in enumerate(moving_entity_position_np):
+                los.append(
+                    utils.check_line_of_sight(
+                        fixed_entity_position_np,
+                        cart_position_np,
+                        WGS84_EARTH_POLAR_RADIUS,
+                    )
                 )
-            )
 
-        # Convert results to ContactInfo
-        time = moving_entity_position_series.time
-        los_array = np.array(los, dtype=bool)
-        return ContactInfo(time, los_array)
+            # Convert results to ContactInfo object
+            time = moving_entity_position_series.time
+            los_array = np.array(los, dtype=bool)
+            return ContactInfo(time, los_array)
